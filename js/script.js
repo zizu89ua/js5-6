8edc8a0acd393f4a01a3a91cc19c18792011b111
Dmytro Kharchenko <dimonkharchenko89@gmail.com>
2017-02-21 16:05:17 +0200

    added folders and files

---
+++ js/script.js
@@ -0,0 +1,38 @@
+
+
+var a = 1;
+setInterval(function(a){
+	for (var i = 0; i < 10; i++) {
+		a;
+	}
+}, 1000);
+console.log(setInterval(a))
+
+
+
+
+
+
+
+// var t=setInterval("tm()",1000);
+// var xt2=0;
+// var xt3=0;
+// function tm() {
+// xt2++;
+// if(xt2>59) {
+// xt3++;
+// xt2=0;
+// } 
+// if(xt2>9) {
+// xt=xt2;
+// } else {
+// xt="0"+xt2;
+// }
+
+// document.getElementById("tm1").innerHTML=xt3+"' "+xt+"\"";
+// }
+// function tm2() {
+// document.getElementById("tm2").innerHTML+=document.getElementById("tm1").innerHTML+" | ";
+// xt++;
+// }
+// </script>
