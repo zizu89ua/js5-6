66ab61434fed32e5cffb3ec9c0ef4562af0f1902
Dmytro Kharchenko <dimonkharchenko89@gmail.com>
2017-02-24 13:54:03 +0200

    added style

--- js/script.js
+++ js/script.js
@@ -1,25 +1,37 @@
 var timer = document.getElementById('timer');
 var startBut = document.getElementById('start-button');
 var resetBut = document.getElementById('reset-button');
-var milsec = document.getElementsByClassName('milsec');
-milsec.style.fontSize = '10px';
+
 var watch = new Stopwatch(timer);
 
 startBut.addEventListener("click", function() {
 	if(watch.isOn){
 		watch.stop();
+		startBut.textContent = 'Cont..';
+		startBut.style.background = 'linear-gradient(135deg, rgba(20,167,20,1) 0%, rgba(20,167,20,1) 42%, rgba(20,167,20,1) 54%, rgba(18,146,18,1) 55%, rgba(21,173,21,1) 100%)';
+
 	} else {
 		watch.start();
+		startBut.textContent = 'Pause';
+		startBut.style.background = 'linear-gradient(135deg, rgba(17,109,170,1) 0%, rgba(17,109,170,1) 38%, rgba(17,109,170,1) 42%, rgba(23,113,188,1) 42%, rgba(23,113,188,1) 46%, rgba(17,109,170,1) 100%)';
 	}
-});
+resetBut.addEventListener('click', function(){
+		watch.reset();
+		startBut.textContent = 'Start';
 
+})
+    
+});
 function Stopwatch(elem) {
 	var time = 0;
 	var interval;
 	var offset;
 
 	function update() {
-		time += delta();
+		if(this.isOn){
+			time += delta();
+		}
+		
 		var formateTime = timeFormate(time);
 		elem.textContent = formateTime;
 	};
@@ -35,6 +47,7 @@ function Stopwatch(elem) {
         var minutes = time.getUTCMinutes().toString();
         var seconds = time.getUTCSeconds().toString();
 		var millsec = time.getMilliseconds().toString();
+		
 		if(hours.length < 2) {
 			hours = '0' + hours; 
 		}
@@ -49,11 +62,10 @@ function Stopwatch(elem) {
 		}
 		return hours + ' : ' + minutes + ' : ' + seconds + ' : ' + millsec;
 	};
-
 	this.isOn = false;
 	this.start = function() {
 		if(!this.isOn) {
-			interval = setInterval(update ,10);
+			interval = setInterval(update.bind(this) ,10);
 			offset = Date.now();
 			this.isOn = true;
 		}
@@ -66,117 +78,9 @@ function Stopwatch(elem) {
 		}
 	};
 	this.reset = function() {
-		time = 0;
+			time = 0;
+			update();
 	};
 };
 
 
-
-// first test
-
-
-// var status = 0;
-// var time = 0;
-
-// function start(){
-// 	status = 1;
-// 	document.getElementById('startButton').disabled = true;
-// 	timer();
-// }
-// function stop(){
-// 	status = 0;
-// 	document.getElementById('startButton').disabled = false;
-// }
-// function reset(){
-// 	status = 0;
-// 	timer = 0;
-// 	document.getElementById('startButton').disabled = false;
-// 	document.getElementById('timer--box').innerHTML = '00:00:00'
-// }
-// function timer(){
-// 	if(status == 1){
-// 		setTimeout(function(){
-// 			time++;
-// 			var hou = Math.floor(time/100/3600);
-// 			var min = Math.floor(time/100/60);
-// 			var sec = Math.floor(time/100);
-// 			var mSec = time % 100;
-// 			if(hou < 10){
-// 				hou = '0' + hou;
-// 			}
-// 			if(min < 10){
-// 				min = '0' + min;
-// 			}
-// 			if(sec >= 60){
-// 				sec = sec % 60;
-// 			}
-// 			if(sec < 10){
-// 				sec = '0' + sec;
-// 			}
-
-// 			document.getElementById('timer--box').innerHTML = hou + ':' +min + ':' + sec + ':' + mSec;
-// 			timer();
-// 		}, 10)
-// 	}
-// }
-
-
-
-// //объявляем переменные
-// var base = 60; 
-// var clocktimer,dateObj,dh,dm,ds,ms; 
-// var readout=''; 
-// var h=1,m=1,tm=1,s=0,ts=0,ms=0,init=0; 
-// //функция для очистки поля
- 
-// //функция для старта секундомера
-// function StartTIME() { 
-// 	var cdateObj = new Date(); 
-// 	var t = (cdateObj.getTime() - dateObj.getTime())-(s*1000); 
-// 	if (t>999) { s++; } 
-// 	if (s>=(m*base)) { 
-// 		ts=0; 
-// 		m++; 
-// 	} else { 
-// 		ts=parseInt((ms/100)+s); 
-// 		if(ts>=base) { ts=ts-((m-1)*base); } 
-// 	} 
-// 	if (m>(h*base)) { 
-// 		tm=1; 
-// 		h++; 
-// 	} else { 
-// 		tm=parseInt((ms/100)+m); 
-// 		if(tm>=base) { tm=tm-((h-1)*base); } 
-// 	} 
-// 	ms = Math.round(t/10); 
-// 	if (ms>99) {ms=0;} 
-// 	if (ms==0) {ms='00';} 
-// 	if (ms>0&&ms<=9) { ms = '0'+ms; } 
-// 	if (ts>0) { ds = ts; if (ts<10) { ds = '0'+ts; }} else { ds = '00'; } 
-// 	dm=tm-1; 
-// 	if (dm>0) { if (dm<10) { dm = '0'+dm; }} else { dm = '00'; } 
-// 	dh=h-1; 
-// 	if (dh>0) { if (dh<10) { dh = '0'+dh; }} else { dh = '00'; } 
-// 	readout = dh + ':' + dm + ':' + ds + '.' + ms; 
-// 	document.MyForm.stopwatch.value = readout; 
-// 	clocktimer = setTimeout("StartTIME()",1); 
-// } 
-// //Функция запуска и остановки
-// function StartStop() { 
-// 	if (init==0){ 
-// 		ClearСlock();
-// 		dateObj = new Date(); 
-// 		StartTIME(); 
-// 		init=1; 
-// 	} else { 
-// 		clearTimeout(clocktimer);
-// 		init=0;
-// 	} 
-// }
-//  function ClearСlock() { 
-// 	clearTimeout(clocktimer); 
-// 	h=1;m=1;tm=1;s=0;ts=0;ms=0; 
-// 	init=0;
-// 	readout='00:00:00.00'; 
-// 	document.MyForm.stopwatch.value=readout; 
-// }
